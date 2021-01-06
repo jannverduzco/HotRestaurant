1dc0647106c6b47493530d13a97f27b69b79d35f
Janneth Verduzco <janneth_verduzco@live.com>
2021-01-05 23:23:09 -0600

    added current wait limit to 5. guests will be moved to waitlist

--- server.js
+++ server.js
@@ -13,51 +13,50 @@ var waitlist = [];
 var clear = [];
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
-app.get("/home", function (req, res) {
+app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
-app.get("/makeres", function (req, res) {
+app.get("/makeres", function(req, res) {
     res.sendFile(path.join(__dirname, "makeres.html"));
 });
-app.get("/viewres", function (req, res) {
+app.get("/viewres", function(req, res) {
     res.sendFile(path.join(__dirname, "viewres.html"));
 });
 
 // Displays guests object
 
-app.get("/api/guests", function (req, res) {
+app.get("/api/guests", function(req, res) {
     return res.json(guests);
 });
 
-app.get("/api/waitlist", function (req, res) {
+app.get("/api/waitlist", function(req, res) {
     return res.json(waitlist);
 });
 
-app.get("/api/clear", function (req, res) {
+app.get("/api/clear", function(req, res) {
     return res.json(clear);
 });
 
 // Create New Characters - takes in JSON input
 
-app.post("/api/guests", function (req, res) {
+app.post("/api/guests", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newGuests = req.body;
-    console.log(newGuests);
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newGuests.routeName = newGuests.name.replace(/\s+/g, "").toLowerCase();
 
-    guests.push(newGuests);
-
-    res.json(newGuests);
-
+    if (guests.length < 5) {
+        guests.push(newGuests);
+        res.json(true);
+    } else {
+        waitlist.push(newGuests);
+        res.json(false);
+    }
 });
 
-app.post("/api/waitlist", function (req, res) {
+app.post("/api/waitlist", function(req, res) {
 
     var newGuests = req.body;
 
@@ -68,6 +67,6 @@ app.post("/api/waitlist", function (req, res) {
     res.json(newGuests);
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
